e95d1f350d08ac155c3bb486986305d64ead0611
Andrew Wang <Andrewruujin@gmail.com>
2023-09-18 17:11:14 -0400

    configure for render

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+    has_many :novels
+    has_many :translators through :novels
+    has_many :publishers through :novels
+    
+    validates :dispaly_name, presence: true
+    validates :email, presence: true, uniqueness: true
+
+    has_secure_password
+
 end
