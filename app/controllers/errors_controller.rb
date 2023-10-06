0afeb2adc1c5adb70fe9df1897074ed621243d33
Andrew Wang <Andrewruujin@gmail.com>
2023-10-06 18:51:48 -0400

    update handle 404 test6

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -2,10 +2,9 @@ class ErrorsController < ApplicationController
   skip_before_action :authorize
 
   def not_found
-    puts "#{Rails.root}"
-    puts 'hello world'
    
-    render file: "#{Rails.root}/src/client/src/App.js", layout: false
+    render file: "#{Rails.root}/client/src/App.js", layout: false
+    
   end
 
   def internal_server_error
