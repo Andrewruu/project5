a0b9b316ee75201eb27a085ab2efa55d19b184da
Andrew Wang <Andrewruujin@gmail.com>
2023-10-05 22:08:38 -0400

    update handle notfound

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux'; // Import Provider
-import store from './store'; // Import your Redux store
+import { Provider } from 'react-redux'; 
+import store from './store';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'errors/not_found'
 
   resources :translators, only: [:index]
   resources :publishers, only: [:index]
@@ -11,4 +12,5 @@ Rails.application.routes.draw do
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
 
+  get '*unmatched_route', to: 'errors#not_found'
 end
